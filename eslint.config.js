010a4b483c9beb012ae135364a48ba3e256e3340
Fernando Gallo <fernandgallo@gmail.com>
2026-09-22 16:17:09 -0400

    chore: add Prettier and lint script

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
 export default defineConfig(
   js.configs.recommended,
   tseslint.configs.recommended,
+  prettier,
 );
