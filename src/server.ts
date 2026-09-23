9848ebe54689bbf9ed93172cb1ccd116cc796c03
Fernando Gallo <fernandgallo@gmail.com>
2026-09-23 10:12:51 -0400

    feat: add Express server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const port = Number(process.env.PORT ?? 3000);
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
